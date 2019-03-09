9fc38886bcea0c5078556729427096cea08aeab4
lee-cooper <cooperlee372@gmail.com>
2019-03-09 14:37:13 -0500

    remove extra dependencies

--- chillbot.js
+++ chillbot.js
@@ -7,8 +7,6 @@ logger.add(new logger.transports.Console, {
     colorize: true
 });
 
-global.servers = {};
-
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -26,7 +24,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-        
+
         args = args.splice(1);
         switch(cmd) {
             // !ping
@@ -40,7 +38,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'greet':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Welcome to the Discord, ya filthy animal'
+                    message: 'Welcome to the Discord' + message.author + ', ya filthy animal'
                 });
                 break;
 
