9321c54d14d36b621fb656c83336b475a35854fa
lee-cooper <cooperlee372@gmail.com>
2019-03-09 18:39:17 -0500

    dice roll enhancement

--- commands/simple/dice-roll.js
+++ commands/simple/dice-roll.js
@@ -11,29 +11,9 @@ class DiceRollCommand extends Discord.Command{
     }
 
     async run(message, args){
-        let roll = Math.floor(Math.random() * (6) + 1);
+        let diceRoll = Math.floor(Math.random() * (6) + 1);
 
-        switch(roll)
-        {
-            case 1:
-                message.reply('You rolled a 1');
-                break;
-            case 2:
-                message.reply('You rolled a 2');
-                break;
-            case 3:
-                message.reply('You rolled a 3');
-                break;
-            case 4:
-                message.reply('You rolled a 4');
-                break;
-            case 5:
-                message.reply('You rolled a 5');
-                break;
-            case 6:
-                message.reply('You rolled a 6');
-                break;
-        }
+        message.reply('You rolled a ' + diceRoll);
     }
 }
 
